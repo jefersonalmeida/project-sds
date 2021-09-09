24e170ed838aa0f4f3e90be37a44325051735623
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-09-09 00:01:51 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
+
+const App = () => {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+      <>
+        <Navbar/>
+        <div className="container">
+          <h1 className="text-primary">Olá mundo!</h1>
+          
+          <DataTable/>
+        </div>
+        <Footer/>
+      </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  return (
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">
+            App desenvolvido por
+            <a href="https://github.com/jefersonalmeida" target="_blank" rel="noreferrer">
+              Jeferson Almeida
+            </a>
+          </p>
+          <p className="text-light">
+            <small><strong>Semana Spring React</strong><br/>
+              Evento promovido pela escola DevSuperior:
+              <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">
+                @devsuperior.ig
+              </a>
+            </small>
+          </p>
+        </div>
+      </footer>
+  );
+};
+
+export default Footer;
