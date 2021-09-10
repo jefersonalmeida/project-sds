7cef521df040755e9b50db118bf89b3ad428e624
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-09-10 17:20:52 -0400

    Ajustes no rodapé

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,16 +3,19 @@ const Footer = () => {
       <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
           <p className="text-light">
-            App desenvolvido por
-            <a href="https://github.com/jefersonalmeida" target="_blank" rel="noreferrer">
-              Jeferson Almeida
-            </a>
+            App desenvolvido por <a
+              href="https://github.com/jefersonalmeida"
+              target="_blank"
+              rel="noreferrer">
+            Jeferson Almeida
+          </a>
           </p>
           <p className="text-light">
             <small><strong>Semana Spring React</strong><br/>
-              Evento promovido pela escola DevSuperior:
-              <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">
-                @devsuperior.ig
+              Evento promovido pela escola DevSuperior: <a
+                  href="https://instagram.com/devsuperior.ig"
+                  target="_blank"
+                  rel="noreferrer">@devsuperior.ig
               </a>
             </small>
           </p>
